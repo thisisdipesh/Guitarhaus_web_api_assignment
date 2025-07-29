48618b683918f72f754d16d7606680f32d0b8aa0
bardan069 <your-email@example.com>
2025-07-29 14:47:07 +0545

    stripe payment

--- guitarhaus_backend/controllers/GuitarController.js
+++ guitarhaus_backend/controllers/GuitarController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("../middleware/async");
 const Guitar = require("../models/Guitar");
 const { protect, authorize } = require("../middleware/auth");
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || ' ');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // @desc    Get all guitars
 // @route   GET /api/v1/guitars
